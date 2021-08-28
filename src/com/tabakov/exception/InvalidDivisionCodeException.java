1b2ce757d5bb01695c6bbc45877904893ed20064
Tabakov Pavel <catchexception@mail.ru>
2021-08-28 13:48:42 +0300

    added InvalidDivisionCodeException.java doc

--- src/com/tabakov/exception/InvalidDivisionCodeException.java
+++ src/com/tabakov/exception/InvalidDivisionCodeException.java
@@ -1,5 +1,9 @@
 package com.tabakov.exception;
 
+/**
+ * Thrown if division name doesn't match defined
+ * in {@link com.tabakov.util.DivisionValidator#DIVISION_REGEX} rule
+ */
 public class InvalidDivisionCodeException extends Exception {
     public InvalidDivisionCodeException(String message) {
         super(message);
